4dfe2edf7b7fa096e0d701ad3b9cba53e1f858d4
LeonathaTurtle <mancillh@gmail.com>
2024-09-23 13:00:27 -0500

    created user model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+const reactionSchema = require('./Reaction');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: () => Promise.resolve(false),
+        message: 'Email validation failed'
+      }
+    },
+    thoughts: [thoughtSchema],
+    friends: [userSchema],
+  },
+  {
+    toJSON: {
+        virtuals: true,
+      },
+  }
+);
+
+// Create a virtual property `friendCount` that retrieves the length of the user's friends array field on query.
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+  });
+
+const User = model('user', userSchema);
+
+module.exports = User;
