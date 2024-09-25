ae8dceebb1e1f205990af6cba8c528fd225eb052
LeonathaTurtle <mancillh@gmail.com>
2024-09-25 09:25:16 -0500

    set up indexes for initial part of routes

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const reactionRoutes = require('./reactionRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+router.use('/users', userRoutes);
+router.use('/reactions', reactionRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
