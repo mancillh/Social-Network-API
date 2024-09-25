b34f05bc74f0467ebbf85b5abeebd2a98d4f30ec
LeonathaTurtle <mancillh@gmail.com>
2024-09-25 18:37:53 -0500

    added routes to userRoutes.js and thoughtRoutes.js, deleted reactionRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,13 +4,18 @@ const {
   getSingleUser,
   createUser,
   updateUser,
-  deleteUser
+  deleteUser,
+  addFriend, 
+  deleteFriend
 } = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
-// /api/users/:userId
-router.route('/:userId').get(getSingleUser).update(updateUser).delete(deleteUser);
+// /api/users/:userId -- maybe $match
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
