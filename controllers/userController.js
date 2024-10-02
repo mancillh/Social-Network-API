84b2e3f5b45eeacd9e02155ee532b64cc1902cce
LeonathaTurtle <mancillh@gmail.com>
2024-10-02 14:17:18 -0500

    delete user working

--- controllers/userController.js
+++ controllers/userController.js
@@ -68,7 +68,7 @@ module.exports = {
     // Delete a user
     async deleteUser(req, res) {
         try {
-            const user = await User.findOneAndRemove({ _id: req.params.studentId });
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
 
             if (!user) {
                 return res.status(404).json({ message: 'No such user exists' });
