ed3c8584fb065fae3cff1e32f21297387acfdc7c
LeonathaTurtle <mancillh@gmail.com>
2024-10-04 06:07:49 -0500

    removed unnecessary comment

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -12,7 +12,7 @@ const {
 // /api/thoughts
 router.route('/').get(getAllThoughts).post(createThought);
 
-// /api/thoughts/:thoughtId -- maybe $match
+// /api/thoughts/:thoughtId 
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -19,3 +19,4 @@ router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
+
